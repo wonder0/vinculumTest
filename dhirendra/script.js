7079f8094e56e211876e2d9c348118729960bad7
Kumar Mridul <kmkinglucky@gmail.com>
2022-04-05 14:11:43 +0530

    Testing

---
+++ dhirendra/script.js
@@ -0,0 +1,29 @@
+
+const whatsAppButton = document.querySelectorAll('[whatsapp]')
+const mailButton = document.querySelectorAll('[mail]')
+
+whatsAppButton.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        window.location.href = "https://web.whatsapp.com/send?phone=+917358324216";
+        
+    })
+})
+
+mailButton.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        window.location.href = "mailto:kmkinglucky@gmail.com";
+        
+    })
+})
+
+
+
+
+
+//document.querySelectorAll('.whatsapp').forEach(button=>{
+//    button.addEventListener('click', ()=>{
+//      window.location.href = "http://www.google.com";
+//  })
+//})
+
+
