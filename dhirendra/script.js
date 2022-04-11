5cbec31ad5d2a9c349ba1b173a62678ee9423ee6
Kumar Mridul <kmkinglucky@gmail.com>
2022-04-11 13:58:15 +0530

    Updating the target content

--- dhirendra/script.js
+++ dhirendra/script.js
@@ -1,17 +1,34 @@
 
 const whatsAppButton = document.querySelectorAll('[whatsapp]')
 const mailButton = document.querySelectorAll('[mail]')
+const instaButton = document.querySelectorAll('[insta]')
+const fbButton = document.querySelectorAll('[fb]')
 
 whatsAppButton.forEach(button =>{
     button.addEventListener('click', ()=>{
-        window.location.href = "https://api.whatsapp.com/send?phone=+917358324216";
+        window.location.href = "https://api.whatsapp.com/send?phone=+918840056261";
         
     })
 })
 
 mailButton.forEach(button =>{
     button.addEventListener('click', ()=>{
-        window.location.href = "mailto:kmkinglucky@gmail.com";
+        window.location.href = "mailto:sssdhirendrad93@gmail.com";
+        
+    })
+})
+
+instaButton.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        window.location.href = "https://www.instagram.com/mr_dhirendra_dubey/?igshid=YmMyMTA2M2Y=";
+        
+    })
+})
+
+
+fbButton.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        window.location.href = "https://www.facebook.com/dhirendra.dubey.71216";
         
     })
 })
